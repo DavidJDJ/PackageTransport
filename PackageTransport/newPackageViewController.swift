fd71d4d8c2c0e92433d332381d58e973b53242f4
Sim <simranlitt@gmail.com>
2015-09-16 01:54:34 -0700

    added request to get current location and get current location in form of coordinates

--- PackageTransport/newPackageViewController.swift
+++ PackageTransport/newPackageViewController.swift
@@ -7,21 +7,37 @@
 //
 
 import UIKit
+import CoreLocation
  
-class newPackageViewController: UIViewController {
+class newPackageViewController: UIViewController, CLLocationManagerDelegate {
+    
+    //variables for getting current location
+    var locationManager = CLLocationManager()
+    var myPosition = CLLocationCoordinate2D()
+    
+    //label and button for showing coordinates
+    
+    @IBOutlet weak var LocationLabel: UILabel!
+    
+    @IBAction func getLocationButtonPressed(sender: UIButton) {
+        
+        locationManager.startUpdatingLocation()
+    }
+    
+    //variables for package location and details
     var packageLocation: String = ""
     var packageDetails: String = ""
-    
     @IBOutlet weak var PackageLocationTextField: UITextField!
-    
     @IBOutlet weak var PackageDetailsTextField: UITextField!
-    
     @IBOutlet weak var PackageLocationLabel: UILabel!
     @IBOutlet weak var PackageDetailsLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         if packageLocation != "" && packageDetails != "" {
             PackageLocationLabel.text = packageLocation
             PackageDetailsLabel.text = packageDetails
@@ -30,15 +46,25 @@ class newPackageViewController: UIViewController {
     }
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "PackageLocationNextButton" {
-            var controller = segue.destinationViewController as! UINavigationController
+            let controller = segue.destinationViewController as! UINavigationController
             let addEventViewController = controller.topViewController as! newPackageViewController
             addEventViewController.packageLocation = PackageLocationTextField.text!
         } else if segue.identifier == "PackageDetailsNextButton" {
-            var controller = segue.destinationViewController as! UINavigationController
+            let controller = segue.destinationViewController as! UINavigationController
             let addEventViewController = controller.topViewController as! newPackageViewController
             addEventViewController.packageLocation = packageLocation
             addEventViewController.packageDetails = PackageDetailsTextField.text!
         }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
+        
+        myPosition = newLocation.coordinate
+        locationManager.stopUpdatingLocation()
+        
+        LocationLabel.text = " \(newLocation.coordinate.latitude) , \(newLocation.coordinate.longitude) "
+        
+        
     }
     
 }
