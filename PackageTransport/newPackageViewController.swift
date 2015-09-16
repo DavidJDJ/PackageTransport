cb42c14b2ee3679eadc9279f6d37cb5b2a020c5f
Sim <simranlitt@gmail.com>
2015-09-16 16:38:23 -0700

    added comments, map view, and pin drop on current location for adding a new package

--- PackageTransport/newPackageViewController.swift
+++ PackageTransport/newPackageViewController.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 import CoreLocation
+import MapKit
  
 class newPackageViewController: UIViewController, CLLocationManagerDelegate {
     
     //variables for getting current location
     var locationManager = CLLocationManager()
     var myPosition = CLLocationCoordinate2D()
+    var locCoord = CLLocationCoordinate2D()
     
     //label and button for showing coordinates
     
@@ -32,18 +34,27 @@ class newPackageViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var PackageLocationLabel: UILabel!
     @IBOutlet weak var PackageDetailsLabel: UILabel!
     
+    //package location map
+    @IBOutlet weak var PackageLocationMapView: MKMapView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //request authorization and start updating location
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        
+        
+        //making sure labels aren't empty
         if packageLocation != "" && packageDetails != "" {
             PackageLocationLabel.text = packageLocation
             PackageDetailsLabel.text = packageDetails
         }
         
     }
+    //setting up segues
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "PackageLocationNextButton" {
             let controller = segue.destinationViewController as! UINavigationController
@@ -57,13 +68,27 @@ class newPackageViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
+    
     func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
         
+        //getting current location and stopping location update
         myPosition = newLocation.coordinate
         locationManager.stopUpdatingLocation()
         
         LocationLabel.text = " \(newLocation.coordinate.latitude) , \(newLocation.coordinate.longitude) "
         
+        //setting the zoom on current location
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(newLocation.coordinate, span)
+        PackageLocationMapView.setRegion(region, animated: true)
+        
+        //dropping a pin on current location
+        let locCoord = myPosition
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locCoord
+        annotation.title = "My Location"
+        annotation.subtitle = "Pickup Location"
+        PackageLocationMapView.addAnnotation(annotation)
         
     }
     
